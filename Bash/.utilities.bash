8cf1ceacb6ba6e3964e7792e95dad1eadfae1910
Rishi K. <contact.rishikmr@gmail.com>
2024-08-07 09:54:30 +0530

    Add `efile` function to quickly edit files using FZF and Neovim
    - Implement new `efile` function in .utilities.bash
    - Use FZF to display and select files interactively
    - Open selected file in Neovim for editing

--- Bash/.utilities.bash
+++ Bash/.utilities.bash
@@ -4,3 +4,9 @@ if [[ $(whereis -b fzf | awk '{print $2}') = *fzf ]]; then
 		git log --oneline --color=always | fzf --layout=reverse-list --preview="git show {1} --color=always" --ansi
 	}
 fi
+
+# search & edit files
+efile() {
+	local filename="$( find -L . -type f | fzf --preview="bat --color=always {}" )"
+	[[ ! -z "${filename}" ]] && ${EDITOR} "${filename}"
+}
