b4f991cb73a637c03d501fef74f58ae74e1703d3
Rishi K. <contact.rishikmr@gmail.com>
2024-08-15 20:51:51 +0530

    Bash: show failure exit code in prompt
    Display exit code in the prompt in case of a failure (non-zero).

--- Bash/.bashrc.d/prompt.sh
+++ Bash/.bashrc.d/prompt.sh
@@ -7,6 +7,16 @@
 [[ -f "$HOME/.config/bash/git-prompt.sh" ]] && source "$HOME/.config/bash/git-prompt.sh" && export GIT_PS1_SHOWDIRTYSTATE=1
 
 SetPS1() {
+	local __EXIT_CODE=" $? "
+	[[ "${__EXIT_CODE}" -le 0 ]] && __EXIT_CODE=""
+
+	local red_bold && red_bold="\033[1;38;2;255;0;0m"
+	local cyan_bold && cyan_bold="\033[1;38;2;0;170;170m"
+	local green_bold && green_bold="\033[1;38;2;0;170;0m"
+	local blue_bold && blue_bold="\033[1;38;2;100;120;255m"
+	local grey_bold && grey_bold="\033[1;38;2;127;127;127m"
+	local col_rst && col_rst="\e[0m"
+
 	local git_root_dir
 	git_root_dir="$( git rev-parse --show-toplevel 2> /dev/null )"
 	git_root_dir="${git_root_dir##\/*\/}"
@@ -20,13 +30,7 @@ SetPS1() {
 	local non_git_parent_path && non_git_parent_path="${PWD%%${git_curr_dir}}"
 	non_git_parent_path=${non_git_parent_path/#${HOME}/\~}
 
-	local cyan_bold && cyan_bold="\033[1;38;2;0;170;170m "
-	local green_bold && green_bold="\033[1;38;2;0;170;0m"
-	local blue_bold && blue_bold="\033[1;38;2;100;120;255m"
-	local grey_bold && grey_bold="\033[1;38;2;127;127;127m"
-	local col_rst && col_rst="\e[0m"
-
-	PS1="${cyan_bold}\A${col_rst} ${green_bold}\u${col_rst} ${grey_bold}${non_git_parent_path}${col_rst}${blue_bold}${git_curr_dir}${col_rst} $(__git_ps1 "(\e[01;33m%s\e[0m)")\$ "
+	PS1="${red_bold}${__EXIT_CODE}${col_rst}${cyan_bold}\A${col_rst} ${green_bold}\u${col_rst} ${grey_bold}${non_git_parent_path}${col_rst}${blue_bold}${git_curr_dir}${col_rst} $(__git_ps1 "(\e[01;33m%s\e[0m)")\$ "
 }
 
 export PROMPT_COMMAND="SetPS1"
