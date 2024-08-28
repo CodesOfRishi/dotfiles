de6c4bd576b1639c4da4f6fe3966edb60740c812
Rishi K. <contact.rishikmr@gmail.com>
2024-08-28 19:22:38 +0530

    Bash: change PS1 prompt style
    - Remove user name from the prompt.
    - Keep oneline prompt.
    - Change command prompt (input indicator) with a new unicode icon.
    - Change color for the input indicator if the exit code of the last command is greater than 0.
    - Change color for the current time.

--- Bash/.bashrc.d/prompt.sh
+++ Bash/.bashrc.d/prompt.sh
@@ -8,39 +8,21 @@
 
 SetPS1() {
 	local __EXIT_CODE="$? "
-	[[ "${__EXIT_CODE}" -le 0 ]] && __EXIT_CODE=""
+	(( __EXIT_CODE <= 0 )) && __EXIT_CODE=""
 
-	local red_bold && red_bold="\033[1;38;2;255;0;0m"
-	local cyan_bold && cyan_bold="\033[1;38;2;0;170;170m"
-	local green_bold && green_bold="\033[1;38;2;0;170;0m"
-	local blue_bold && blue_bold="\033[1;38;2;100;120;255m"
-	local grey_bold && grey_bold="\033[1;38;2;127;127;127m"
-	local col_rst && col_rst="\e[0m"
+	local git_info="$(__git_ps1 "(\e[01;33m%s\e[0m)")"
+	[[ -n "${git_info}" ]] && git_info=" ${git_info}"
 
-	local git_root_dir
-	git_root_dir="$( git rev-parse --show-toplevel 2> /dev/null )"
-	git_root_dir="${git_root_dir##\/*\/}"
+	local colr_rst && colr_rst="\e[0m"
+	local colr_grey && colr_grey="\033[1;38;2;122;122;122m"
+	local colr_blue && colr_blue="\033[1;38;2;0;150;255m"
+	local colr_cyan && colr_cyan="\033[1;38;2;0;170;170m"
 
-	local git_curr_dir 
-	git_curr_dir="${git_root_dir}/$( git rev-parse --show-prefix 2> /dev/null )"
-	git_curr_dir="${git_curr_dir%\/}"
-
-	unset git_root_dir
-
-	local non_git_parent_path && non_git_parent_path="${PWD%%${git_curr_dir}}"
-	non_git_parent_path=${non_git_parent_path/#${HOME}/\~}
-
-	if [[ -h "${PWD}" ]]; then
-		if [[ -n "${git_curr_dir}" ]]; then
-			PS1="${red_bold}${__EXIT_CODE}${cyan_bold}\A ${green_bold}\u ${grey_bold}${non_git_parent_path}${col_rst} ➜ ${blue_bold}${git_curr_dir}${col_rst}
-$(__git_ps1 "(\e[01;33m%s\e[0m)")\$ "
-		else
-			PS1="${red_bold}${__EXIT_CODE}${cyan_bold}\A ${green_bold}\u ${grey_bold}${non_git_parent_path}${col_rst} ➜ ${grey_bold}$( readlink "${PWD}" )${col_rst}
-$(__git_ps1 "(\e[01;33m%s\e[0m)")\$ "
-		fi
+	if [[ -n "${__EXIT_CODE}" ]]; then
+		local colr_red && colr_red="\033[1;38;2;255;0;0m"
+		PS1="${colr_red}${__EXIT_CODE}${colr_grey}\A ${colr_blue}\w${colr_rst}${git_info} ${colr_red}❱${colr_rst} "
 	else
-		PS1="${red_bold}${__EXIT_CODE}${cyan_bold}\A ${green_bold}\u ${grey_bold}${non_git_parent_path}${blue_bold}${git_curr_dir}${col_rst}
-$(__git_ps1 "(\e[01;33m%s\e[0m)")\$ "
+		PS1="${colr_grey}\A ${colr_blue}\w${colr_rst}${git_info} ${colr_cyan}❱${colr_rst} "
 	fi
 }
 
