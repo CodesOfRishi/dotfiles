a8837463d4df6993bad46251919a43bb996d3d75
Rishi K. <contact.rishikmr@gmail.com>
2024-09-05 17:52:28 +0530

    Bash(environment): export `GCC_COLORS` for colored GCC warnings and errors

--- Bash/.bashrc.d/environment.sh
+++ Bash/.bashrc.d/environment.sh
@@ -14,3 +14,6 @@ fi
 # REF: https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 export XDG_CONFIG_HOME="$HOME/.config" 
 export XDG_DATA_HOME="$HOME/.local/share"
+
+# colored GCC warnings and errors
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
