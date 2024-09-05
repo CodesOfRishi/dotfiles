f6f6f7bb35bf6a3ff6222d237b148ad6319cb733
Rishi K. <contact.rishikmr@gmail.com>
2024-09-05 17:33:23 +0530

    Bash(aliases): add aliases for `dir`, `vdir`, `grep`, `fgrep`, `egrep`

--- Bash/.bashrc.d/aliases.sh
+++ Bash/.bashrc.d/aliases.sh
@@ -5,8 +5,16 @@
 alias ll="ls -lshF --color=auto"
 alias la="ls -lashF --color=auto"
 alias lc="ls -lCF --color=auto"
+alias dir="dir --color=auto"
+alias vdir="vdir --color=auto"
 alias mv="mv -i"
 alias cp="cp -i"
 alias rm="rm -i"
 alias less="less --use-color"
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
 
+# # Add an "alert" alias for long running commands.  Use like so:
+# #   sleep 10; alert
+# alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
