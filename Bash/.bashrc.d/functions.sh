89b68b1feb092ef2e84f6bff148e25973e9dd741
Rishi K. <contact.rishikmr@gmail.com>
2024-09-20 15:55:48 +0530

    Bash(functions): upon hitting enter, git show the commit in a pager always in `gh()`

--- Bash/.bashrc.d/functions.sh
+++ Bash/.bashrc.d/functions.sh
@@ -8,7 +8,7 @@ gh() {
 	fzf --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
 		--header 'Press CTRL-S to toggle sort' \
 		--preview='grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | delta -n' \
-		--bind 'enter:execute(grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always)'
+		--bind 'enter:execute(grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | delta -n | less -R)'
 		# --preview='grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' | grep -o "[a-f0-9]\{7,\}"
 		# --preview="git show {1} --color=always" | grep -o "[a-f0-9]\{7,\}"
 }
