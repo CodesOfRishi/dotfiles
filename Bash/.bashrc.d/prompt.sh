6f7dfe6ad514c84669a234a7a30c89890dbffb24
Rishi K. <contact.rishikmr@gmail.com>
2024-09-21 19:36:37 +0530

    Bash(prompt): customize prompt
    - Make two-line prompt.
    - Add a horizontal line in the first-line of the prompt w.r.t. the terminal width.
    - Move git information to the second-line of the prompt with a 'git' nerd-font icon.
    - Change command prompt (input indicator) with a nerd-font arrow icon when the previous command returns a non-zero exit code.

--- Bash/.bashrc.d/prompt.sh
+++ Bash/.bashrc.d/prompt.sh
@@ -11,18 +11,30 @@ SetPS1() {
 	(( __EXIT_CODE <= 0 )) && __EXIT_CODE=""
 
 	local git_info="$(__git_ps1 "(\e[01;33m%s\e[0m)")"
-	[[ -n "${git_info}" ]] && git_info=" ${git_info}"
+	[[ -n "${git_info}" ]] && git_info=" ${git_info} "
 
 	local colr_rst && colr_rst="\e[0m"
 	local colr_grey && colr_grey="\033[1;38;2;122;122;122m"
 	local colr_blue && colr_blue="\033[1;38;2;0;150;255m"
-	local colr_cyan && colr_cyan="\033[1;38;2;0;170;170m"
+
+	generate-horizontal-line() {
+		local hr_line
+		printf -v hr_line "%*s\n" "${1:-${COLUMNS}}" ""
+		printf "%s\n" "${hr_line// /•}"
+	}
+
+	local _pwd="${PWD/#$HOME/\~}"
+	# curr_width = size_of(exitcode + currenttime + pwd + spaces)
+	local curr_width="$(( ${#__EXIT_CODE} + 5 + ${#_pwd} + 2 ))"
 
 	if [[ -n "${__EXIT_CODE}" ]]; then
 		local colr_red && colr_red="\033[1;38;2;255;0;0m"
-		PS1="${colr_red}${__EXIT_CODE}${colr_grey}\A ${colr_blue}\w${colr_rst}${git_info} ${colr_red}❱${colr_rst} "
+		PS1="${colr_red}${__EXIT_CODE}${colr_grey}\A ${colr_blue}\w${colr_rst}${colr_grey} $(generate-horizontal-line $(( $COLUMNS - $curr_width )))${colr_rst}"
+		PS1="${PS1}\n${git_info}${colr_red}󰁕${colr_rst} "
 	else
-		PS1="${colr_grey}\A ${colr_blue}\w${colr_rst}${git_info} ${colr_cyan}❱${colr_rst} "
+		local colr_cyan && colr_cyan="\033[1;38;2;0;170;170m"
+		PS1="${colr_grey}\A ${colr_blue}\w${colr_rst}${colr_grey} $(generate-horizontal-line $(( $COLUMNS - $curr_width )))${colr_rst}"
+		PS1="${PS1}\n${git_info}${colr_cyan}❱${colr_rst} "
 	fi
 }
 
