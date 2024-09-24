c252df3cd22ad868981bb9613ee483a7756a1745
Rishi K. <contact.rishikmr@gmail.com>
2024-09-24 19:36:35 +0530

    Bash(prompt): move exit code information to right side of first-line prompt
    - Move exit code number information to the right side of the first-line prompt with an `x` icon.
    - The commits, also includes a few refactors.

--- Bash/.bashrc.d/prompt.sh
+++ Bash/.bashrc.d/prompt.sh
@@ -8,7 +8,7 @@
 
 SetPS1() {
 	local __EXIT_CODE="$? "
-	(( __EXIT_CODE <= 0 )) && __EXIT_CODE=""
+	(( __EXIT_CODE <= 0 )) && __EXIT_CODE="" || __EXIT_CODE="✘ ${__EXIT_CODE}"
 
 	local git_info="$(__git_ps1 "(\e[01;33m%s\e[0m)")"
 	[[ -n "${git_info}" ]] && git_info="${git_info} "
@@ -30,11 +30,11 @@ SetPS1() {
 
 	if [[ -n "${__EXIT_CODE}" ]]; then
 		local colr_red && colr_red="\033[1;38;2;255;0;0m"
-		PS1="${colr_red}${__EXIT_CODE}${colr_grey}\A ${colr_blue}\w${colr_rst}${colr_grey} $(generate-horizontal-line $(( $COLUMNS - $curr_width ))) \d${colr_rst}"
+		PS1="${colr_grey}\A ${colr_blue}\w ${colr_grey}$(generate-horizontal-line $(( $COLUMNS - $curr_width ))) ${colr_red}${__EXIT_CODE}${colr_grey}\d${colr_rst}"
 		PS1="${PS1}\n${git_info}${colr_red}󰁕${colr_rst} "
 	else
 		local colr_cyan && colr_cyan="\033[1;38;2;0;170;170m"
-		PS1="${colr_grey}\A ${colr_blue}\w${colr_rst}${colr_grey} $(generate-horizontal-line $(( $COLUMNS - $curr_width ))) \d${colr_rst}"
+		PS1="${colr_grey}\A ${colr_blue}\w ${colr_grey}$(generate-horizontal-line $(( $COLUMNS - $curr_width ))) \d${colr_rst}"
 		PS1="${PS1}\n${git_info}${colr_cyan}❱${colr_rst} "
 	fi
 }
