70e9555d4be25fac0f0c363cf9dfd7a58fe695ac
Rishi K. <contact.rishikmr@gmail.com>
2024-10-24 20:27:45 +0530

    Bash(completions): bind `redraw-current-line` within the completion functions
    Bind `redraw-current-line` (for workaround of visual curruption of the prompt after completion) within the completion functions, not outside of the functions.

--- Bash/.bashrc.d/completions.sh
+++ Bash/.bashrc.d/completions.sh
@@ -2,12 +2,11 @@
 # │ Tab Completions │
 # ╰─────────────────╯
 
-# To redraw line after fzf closes (printf '\e[5n') 
-# This is useful when the terminal is altered by FZF, and the command line gets visually corrupted or misaligned
-bind '"\e[0n": redraw-current-line' 2> /dev/null
-
 # Tab comletion for $EDITOR
 editor_completion() {
+	# To redraw line after fzf closes (printf '\e[5n') 
+	# This is useful when the terminal is altered by FZF, and the command line gets visually corrupted or misaligned
+	bind '"\e[0n": redraw-current-line' 2> /dev/null
 	local selected_result 
 	if selected_result="$(compgen -f -- "${COMP_WORDS[COMP_CWORD]}" | command fzf \
 		--prompt='❯ ' \
