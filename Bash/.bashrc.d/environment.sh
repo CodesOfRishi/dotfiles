a425ea874a5cd25a8752247e6d13dea84120b08d
Rishi K. <contact.rishikmr@gmail.com>
2026-07-23 22:15:46 +0530

    Bash(environment): export `XDG_CACHE_HOME` to `$HOME/.cache`

--- Bash/.bashrc.d/environment.sh
+++ Bash/.bashrc.d/environment.sh
@@ -17,6 +17,7 @@ fi
 export XDG_CONFIG_HOME="$HOME/.config" 
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_CACHE_HOME="$HOME/.cache"
 
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
